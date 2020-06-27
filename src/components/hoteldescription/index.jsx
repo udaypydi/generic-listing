30105921f840060c95422e14866a44b051ce368c
udaypydi <udaypydi333@gmail.com>
2020-06-28 00:21:03 +0530

    fix(hoteldescription): initial loader

--- src/components/hoteldescription/index.jsx
+++ src/components/hoteldescription/index.jsx
@@ -21,87 +21,95 @@ function HotelDescription(props) {
         setEssentials(json.data.essentials);
       });
 
-      if (!hotel.id) {
-        const { history } = props;
-        history.push('/');
-      }
+    if (!hotel.id) {
+      const { history } = props;
+      history.push('/');
+    }
   }, []);
 
   return (
     <>
-        <div
-        className="w-3/4 bg-white shadow-sm m-2 border border-solid flex"
-        >
-        <img src={hotelImage} className="h-auto w-1/2" />
-        <div className="px-5">
-            <p className="text-xl font-bold my-2">{hotel.name}</p>
-            <p className="text text-gray-600 my-2">
-            {hotel.locality}
-            ,
-            {' '}
-            {hotel.city}
-            </p>
-            <HotelTag tag="Free Early Check-In" />
-            {
+      {
+            hotel.id && (
+            <>
+              <div
+                className="w-3/4 bg-white shadow-sm m-2 border border-solid flex"
+              >
+                <img src={hotelImage} className="h-auto w-1/2" />
+                <div className="px-5">
+                  <p className="text-xl font-bold my-2">{hotel.name}</p>
+                  <p className="text text-gray-600 my-2">
+                    {hotel.locality}
+                    ,
+                    {' '}
+                    {hotel.city}
+                  </p>
+                  <HotelTag tag="Free Early Check-In" />
+                  {
                 hotel.price ? (
-                    <p className="text-3xl font-bold my-2">
+                  <p className="text-3xl font-bold my-2">
                     &#8377;
-                    {hotel.price} <span className="text-xl text-green-700">Refundable</span>
-                    </p>
+                    {hotel.price}
+                    {' '}
+                    <span className="text-xl text-green-700">Refundable</span>
+                  </p>
                 ) : <p className="text-red-500 text-base my-2">SOLD OUT</p>
             }
-            {
+                  {
                         hotel.price && (
                         <Button
-                        buttonText="Book Now"
-                        disabled={!hotel.price}
-                        customClass="bg-green-500 text-white p-3 hover:bg-green-700 focus:outline-none my-2"
+                          buttonText="Book Now"
+                          disabled={!hotel.price}
+                          customClass="bg-green-500 text-white p-3 hover:bg-green-700 focus:outline-none my-2"
                         />
                         )
                     }
 
-        </div>
-        </div>
-        <div className="w-3/4">
-            <Tab 
-                tabElements={[ 
+                </div>
+              </div>
+              <div className="w-3/4">
+                <Tab
+                  tabElements={[
                     { name: 'Policies', value: 'POLICIES' },
                     { name: 'Essentials', value: 'ESSENTIALS' },
-                ]}
-                activeTab={activeTab}
-                onTabClick={(element) => setActiveTab(element)}
-            />
-        </div>
-        <div className="w-3/4 bg-white shadow-sm flex p-5 mt-2">
-            {
+                  ]}
+                  activeTab={activeTab}
+                  onTabClick={(element) => setActiveTab(element)}
+                />
+              </div>
+              <div className="w-3/4 bg-white shadow-sm flex p-5 mt-2">
+                {
                 activeTab === 'POLICIES' && (
-                    <div>
-                        <p className="text-2xl font-bold">Policies</p>
-                        <div>
-                            {
+                <div>
+                  <p className="text-2xl font-bold">Policies</p>
+                  <div>
+                    {
                                 policies.map((policy) => (
-                                    <p className="text-gray-500">{policy}</p>
+                                  <p className="text-gray-500">{policy}</p>
                                 ))
                             }
-                        </div>    
-                    </div>
+                  </div>
+                </div>
                 )
             }
-            {
+                {
                 activeTab === 'ESSENTIALS' && (
-                    <div>
-                        <p className="text-2xl font-bold">Essentials</p>
-                        <div>
-                            {
+                <div>
+                  <p className="text-2xl font-bold">Essentials</p>
+                  <div>
+                    {
                                 essentials.map((essential) => (
-                                    <p className="text-gray-500">{essential}</p>
+                                  <p className="text-gray-500">{essential}</p>
                                 ))
                             }
-                        </div>    
-                    </div>
+                  </div>
+                </div>
                 )
             }
-        </div>
+              </div>
+            </>
+            )
+        }
     </>
   );
 }
--- src/components/tab/index.jsx
+++ src/components/tab/index.jsx
@@ -3,29 +3,31 @@ import cx from 'classnames';
 import './tab.scss';
 
 function Tab(props) {
-    const [activeTab, setActiveTab] = useState(props.activeTab);
-    const { tabElements, onTabClick } = props;
+  const [activeTab, setActiveTab] = useState(props.activeTab);
+  const { tabElements, onTabClick } = props;
 
-    return (
-        <div className="bg-white shadow-sm flex HotelDescriptionTab">
-            {
+  return (
+    <div className="bg-white shadow-sm flex HotelDescriptionTab">
+      {
                 tabElements.map((element) => (
-                    <p 
-                        className={cx(
-                            "p-4 cursor-pointer text-gray-500 hover:bg-gray-300",
-                            {
-                                "Element--active": activeTab === element.value,
-                            }
-                        )}
-                        onClick={() => {
-                            onTabClick(element.value);
-                            setActiveTab(element.value);
-                        }}
-                    >{element.name}</p>
+                  <p
+                    className={cx(
+                      'p-4 cursor-pointer text-gray-500 hover:bg-gray-300',
+                      {
+                        'Element--active': activeTab === element.value,
+                      },
+                    )}
+                    onClick={() => {
+                      onTabClick(element.value);
+                      setActiveTab(element.value);
+                    }}
+                  >
+                    {element.name}
+                  </p>
                 ))
             }
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Tab;
